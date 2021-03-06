c0473fab2ad7459c7987ddbd1821a126ff8645cc
sassan <sassan.marouf@gmail.com>
2021-03-06 10:31:36 +0100

    changed footer css

--- src/pageload.js
+++ src/pageload.js
@@ -1,51 +1,46 @@
 const pageLoad = (function() {
 
-    const main = document.querySelector('#main');
+    const main = document.querySelector("#main");
 
     function createHeader() {
-        let header = document.createElement('div');
-        header.id = 'header';
+        let header = document.createElement("div");
+        header.id = "header";
         
-        let title = document.createElement('p');
-        title.id = 'title';
-        title.textContent = 'Kermanshah';
-
-        let description = document.createElement('p');
-        description.id = 'descr';
-        description.textContent = 'Traditional. Iranian. Food.'
+        let title = document.createElement("p");
+        title.id = "title";
+        title.textContent = "Kermanshah";
 
         header.appendChild(title);
-        header.appendChild(description);
         main.appendChild(header);
     }
 
     function createContentDiv() {
-        let contentDiv = document.createElement('div');
-        contentDiv.id = 'content';
+        let contentDiv = document.createElement("div");
+        contentDiv.id = "content";
 
         main.appendChild(contentDiv);
     }
 
     function createNavigation() {
-        let navBar = document.createElement('div');
-        navBar.id = 'navBar';
+        let navBar = document.createElement("div");
+        navBar.id = "navBar";
 
-        let navHome = document.createElement('p');
-        navHome.id = 'navHome';
-        navHome.classList.add('navButton');
-        navHome.textContent = 'Home';
+        let navHome = document.createElement("p");
+        navHome.id = "navHome";
+        navHome.classList.add("navButton");
+        navHome.textContent = "Home";
 
-        let navMenu = document.createElement('p');
-        navMenu.id = 'navMenu';
-        navMenu.classList.add('navButton');
-        navMenu.textContent = 'Menu';
+        let navMenu = document.createElement("p");
+        navMenu.id = "navMenu";
+        navMenu.classList.add("navButton");
+        navMenu.textContent = "Menu";
 
-        let navContact = document.createElement('p');
-        navContact.id = 'navContact';
-        navContact.classList.add('navButton');
-        navContact.textContent = 'Contact';
+        let navContact = document.createElement("p");
+        navContact.id = "navContact";
+        navContact.classList.add("navButton");
+        navContact.textContent = "Contact";
 
-        let header = document.getElementById('header');
+        let header = document.getElementById("header");
         header.appendChild(navBar);
         navBar.appendChild(navHome);
         navBar.appendChild(navMenu);
@@ -53,10 +48,19 @@ const pageLoad = (function() {
     }
 
     function createFooter() {
-        let footer = document.createElement('div');
-        footer.id = 'footer';
+        let footer = document.createElement("div");
+        footer.id = "footer";
+
+        let footerContent = document.createElement("div");
+        footerContent.id = "footerContent";
+
+        let githubLogo = document.createElement("div");
+        githubLogo.id = "githubLogo";
+        githubLogo.textContent = "built by smd92";
 
         main.appendChild(footer);
+        footer.appendChild(footerContent);
+        footerContent.appendChild(githubLogo);
     }
 
     return {
